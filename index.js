716fb2c94dc9730bf3c020450a72b86f974350ab
Chihuahua <piseth@PISETHs-MacBook-Pro.local>
2023-02-03 20:54:48 +0700

    add env for DB and PORT

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors'
 import bodyParser from 'body-parser';
-
+import dotenv from 'dotenv';
 import prouter from './routes/product.js';
 import erouter from './routes/event.js';
 import authrouter from './routes/auth.js';
-
+dotenv.config();
 const app = express();
-const port = 5001;
+const PORT = process.env.PORT;
 app.use(bodyParser.json({limit:'100mb'}));
 app.use(bodyParser.urlencoded({extended: true, limit: '100mb'}));
 app.use(cors());
@@ -21,11 +21,11 @@ app.get("/", (req, res)=>{
     res.send("APP IS RUNNING...")
 })
 app.get('/*', (req, res)=>{
-    res.sendFile('index.html')
+    res.send('index.html')
 })
 
 mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://lg500ng:KoEbKiMrSg7UXiXi@asp-cluster.arspmut.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@asp-cluster.arspmut.mongodb.net/?retryWrites=true&w=majority`)
 .then(()=> console.log("db connected..."))
 .catch((err) => console.log(err));
-app.listen(port, ()=>console.log("ok"));
+app.listen(PORT, ()=>console.log("ok"));
